18003496499b29cae57d54b1284927b07ca69254
israelramon <israeldeoliveira.passos@gmail.com>
2025-08-11 17:57:04 -0300

    Criado a lista para armazenamento de nomes do amigo secreto.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-//Inicie declarando uma variável do tipo array, que armazenará os nomes dos amigos inseridos.
+//Criar um array para armazenar os nomes
+let amigos = [];
